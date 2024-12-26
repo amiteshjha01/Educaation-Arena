579fe8630aaca73c6baf478a6b83f622f656a8c1
Amitesh Jha <amiteshjha2012@gmail.com>
2024-12-26 19:34:08 +0530

    1 st commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def aboutus():
+    return render_template('about us.html')
+
+@app.route("/contact")
+def contactus():
+    return render_template('contact us.html')
+
+@app.route("/courses")
+def courses():
+    return render_template('courses.html')
+
+@app.route("/register")
+def registration():
+    return render_template('registration.html')
+
+
+
+app.run(debug = True)
